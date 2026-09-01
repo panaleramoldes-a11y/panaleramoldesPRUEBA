319ded38f910a53377be13af02d2ed5955259b1c
panaleramoldes-a11y <panaleramoldes@gmail.com>
2026-09-01 17:52:56 -0300

    Delete config directory

--- config/settings.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def init_page_config():
-    """Configura la disposición visual y título de la ventana."""
-    st.set_page_config(
-        page_title="Pañalera Moldes - ERP",
-        page_icon="🛡️",
-        layout="wide"
-    )
