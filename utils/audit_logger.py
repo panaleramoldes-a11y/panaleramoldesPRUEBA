f56bcbc2619e1f2499a08ae78ce14ce91126addb
panaleramoldes-a11y <panaleramoldes@gmail.com>
2026-09-01 17:53:46 -0300

    Delete utils directory

--- utils/audit_logger.py
+++
@@ -1,6 +0,0 @@
-"""
-utils/audit_logger.py
-Alias de compatibilidad para redirigir la auditoría a services.audit_service.
-"""
-
-from services.audit_service import log_auditoria
--- utils/supabase_client.py
+++
@@ -1,6 +0,0 @@
-"""
-utils/supabase_client.py
-Alias de compatibilidad para redirigir a config.database.
-"""
-
-from config.database import db, get_supabase_client
