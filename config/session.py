efafebc5aeb9beb00f341cef636fc5bec676a9c3
panaleramoldes-a11y <panaleramoldes@gmail.com>
2026-09-01 12:10:22 -0300

    Update session.py

--- config/session.py
+++ config/session.py
@@ -1,18 +1,23 @@
 import streamlit as st
 
 def init_session_state():
-    """Inicializa los valores por defecto del estado de sesión."""
-    if "logeado" not in st.session_state:
-        st.session_state.logeado = False
+    """
+    Inicializa los valores por defecto del estado de sesión en un solo lugar.
+    """
+    if "autenticado" not in st.session_state:
+        st.session_state.autenticado = False
 
     if "usuario_actual" not in st.session_state:
         st.session_state.usuario_actual = None
+
+    if "vendedor_id" not in st.session_state:
+        st.session_state.vendedor_id = None
 
     if "rol" not in st.session_state:
         st.session_state.rol = None
 
     if "lista_global_vta" not in st.session_state:
-        st.session_state.lista_global_vta = []
+        st.session_state.lista_global_vta = "Automática (P1/P2)"
 
     if "carrito_compra" not in st.session_state:
         st.session_state.carrito_compra = []
